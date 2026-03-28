658466dafe80761ce557f891ee9b444f44f63d0e
Owen Kearney <meepowen123@gmail.com>
2026-03-28 14:07:18 -0400

    Completed Lab 9

---
+++ mywork/lab9/ec2-info.sh
@@ -0,0 +1,8 @@
+aws ec2 describe-instances | jq '.Reservations[].Instances[] | {
+  ImageId,
+  InstanceId,
+  InstanceType,
+  InstanceName: (.Tags[]? | select(.Key=="Name") | .Value),
+  PublicIpAddress,
+  State,
+}'
